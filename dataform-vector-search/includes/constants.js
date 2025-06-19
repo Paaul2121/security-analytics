49ecfdefef096e052679412039ca7232f8f53998
Paul-Cristian Costea <paulcristianc@google.com>
2025-06-19 14:20:12 +0000

    yes

--- dataform-vector-search/includes/constants.js
+++
@@ -1,4 +0,0 @@
- const PROJECT_ID = dataform.projectConfig.vars.logs_export_project;
- const DATASET_ID = dataform.projectConfig.vars.logs_export_dataset;
- 
- module.exports = { PROJECT_ID, DATASET_ID };
